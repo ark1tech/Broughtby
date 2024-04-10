cfe91b035096b6f0fc562b25273fa121bff74203
RK Mañago <rbmanago1@up.edu.ph>
2024-04-10 20:52:54 +0800

    fix typo again

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,7 +7,7 @@
 	import Receipt from '$lib/receipt.svg';
 	import Slow from '$lib/zzz.svg';
 	import Puzzle from '$lib/mood-puzzled.svg';
-	import Form from './Form.svelte';
+	import Form from './form.svelte';
 
 	const Right_Arrow = `<svg  xmlns="http://www.w3.org/2000/svg"  width="20"  height="20"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-arrow-right"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M5 12l14 0" /><path d="M13 18l6 -6" /><path d="M13 6l6 6" /></svg>`;
 
