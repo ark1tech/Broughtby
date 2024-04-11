bb44324b7267ce29f3ee739678f24725e4bf4c5a
RK Mañago <rbmanago1@up.edu.ph>
2024-04-11 13:03:00 +0800

    fix: clean logo

--- src/routes/NavBar.svelte
+++ src/routes/NavBar.svelte
@@ -1,5 +1,5 @@
 <script>
-	import logo from '$lib/broughtby3.svg';
+	import logo from '$lib/broughtby4.svg';
 	import Button from './Button.svelte';
 
 	const Hamburger_Menu = `<svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-menu-2"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M4 6l16 0" /><path d="M4 12l16 0" /><path d="M4 18l16 0" /></svg>`;
